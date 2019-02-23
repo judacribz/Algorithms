2c576f4ed71c905d0afa555a2ed1391a4940377b
judacribz <sheron.balas@gmail.com>
2019-02-23 15:55:25 -0500

    added fast solution for lowest common multiple

--- week2/lcm.cpp
+++ week2/lcm.cpp
@@ -1,16 +1,31 @@
 #include <iostream>
+#include <algorithm>
 
-long long lcm_naive(int a, int b) {
-  for (long l = 1; l <= (long long) a * b; ++l)
+#define LL long long
+
+LL lcm_naive(int a, int b) {
+  for (LL l = 1; l <= (LL) a * b; ++l)
     if (l % a == 0 && l % b == 0)
       return l;
 
-  return (long long) a * b;
+  return (LL) a * b;
+}
+
+LL lcm_fast(int a, int b) {
+    int max = std::max(a, b);
+
+    for (LL l = max; l < (LL) a * b; l+=max) {
+        if (l % a == 0 && l % b == 0) {
+            return l;
+        }
+    }
+
+    return (LL) a * b;
 }
 
 int main() {
   int a, b;
   std::cin >> a >> b;
-  std::cout << lcm_naive(a, b) << std::endl;
+  std::cout << lcm_fast(a, b) << std::endl;
   return 0;
 }
