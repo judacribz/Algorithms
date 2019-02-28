16e4f32118643d6003a43cc59122e82dc9ff3ca6
judacribz <sheron.balas@gmail.com>
2019-02-28 15:58:21 -0500

    fixed issues with week 2 code

--- week2/fibonacci_sum_squares.cpp
+++ week2/fibonacci_sum_squares.cpp
@@ -5,15 +5,17 @@
 
 using namespace std;
 
-int fibonacci_sum_squares_naive(long long n) {
+int fibonacci_sum_squares_naive(long long n)
+{
     if (n <= 1)
         return n;
 
     long long previous = 0;
-    long long current  = 1;
-    long long sum      = 1;
+    long long current = 1;
+    long long sum = 1;
 
-    for (long long i = 0; i < n - 1; ++i) {
+    for (long long i = 0; i < n - 1; ++i)
+    {
         long long tmp_previous = previous;
         previous = current;
         current = tmp_previous + current;
@@ -23,29 +25,33 @@ int fibonacci_sum_squares_naive(long long n) {
     return sum % 10;
 }
 
-long fibonacci_sum_squares_fast(LL n) {
-    if (n <= 1) {
+LL fibonacci_sum_squares_fast(LL n)
+{
+    if (n <= 1)
+    {
         return n;
     }
 
-    long 
+    LL
         a = 0,
-        b  = 1,
+        b = 1,
         period = 60, // pisano period is always 60 for mod 10
         sum = 0,
         temp;
 
-    for (int i = 0; i < n % period; i++) {
-        sum = (sum + (b * b) % 10 ) % 10;
+    for (LL i = 0; i < n % period; i++)
+    {
+        sum = sum + (b * b) % 10;
         temp = b;
-        b = (a + b);
+        b = (a + b) % 10;
         a = temp;
     }
 
-    return sum;
+    return sum % 10;
 }
 
-int main() {
+int main()
+{
     LL n = 0;
     cin >> n;
     cout << fibonacci_sum_squares_fast(n) << endl;
--- week2/lcm.cpp
+++ week2/lcm.cpp
@@ -3,32 +3,34 @@
 
 #define LL long long
 
-LL lcm_naive(int a, int b) {
-  for (LL l = 1; l <= (LL) a * b; ++l)
+using namespace std;
+
+LL lcm_naive(int a, int b)
+{
+  for (LL l = 1; l <= (LL)a * b; ++l)
     if (l % a == 0 && l % b == 0)
       return l;
 
-  return (LL) a * b;
+  return (LL)a * b;
 }
 
-
-LL lcm_fast(int a, int b) {
-    int max = std::max(a, b);
-
-    for (LL l = max; l < (LL) a * b; l+=max) {
-        if (l % a == 0 && l % b == 0) {
-            return l;
-        }
-    }
-
-    return (LL) a * b;
+int gcd_fast(int a, int b)
+{
+  return (b == 0) ? a : gcd_fast(b, a % b);
 }
 
-int main() {
-  int a, b;
+LL lcm_fast(LL a, LL b)
+{
+  return a * b / gcd_fast(a, b);
+}
 
-  std::cin >> a >> b;
-  std::cout << lcm_fast(a, b) << std::endl;
+int main()
+{
+  LL a, b;
+
+  cin >> a;
+  cin >> b;
+  cout << lcm_fast(a, b) << endl;
 
   return 0;
 }
