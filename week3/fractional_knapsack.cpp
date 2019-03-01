0749f767d26bbbb8c5eed352b7584691f0c49e1f
judacribz <sheron.balas@gmail.com>
2019-03-01 15:40:25 -0500

    finished 2 - max value of loot problem

--- week3/fractional_knapsack.cpp
+++ week3/fractional_knapsack.cpp
@@ -1,39 +1,80 @@
 #include <iostream>
 #include <vector>
 #include <map>
+#include <math.h>
 
 using namespace std;
 
-double get_optimal_value(int capacity, vector<int> weights, vector<int> values)
+int getGreatestDensity(map<int, double> densities)
 {
-  double value = 0.0;
-  vector<float> densities;
+  double max = -1;
+  int i = -1;
+  map<int, double>::iterator it = densities.begin();
+
+  for (it; it != densities.end(); it++)
+  {
+    if (it->second > max)
+    {
+      max = it->second;
+      i = it->first;
+    }
+  }
+
+  return i;
+}
+
+double get_optimal_value(long capacity, vector<long> weights, vector<long> values)
+{
+  double totalValue = 0.0;
+  map<int, double> densities;
 
   for (int i = 0; i < weights.size(); i++)
   {
-    densities[i] = (float)values[i] / weights[i];
+    densities[i] = (double)values[i] / weights[i];
   }
 
-  cout << densities << endl;
+  int
+      index = -1,
+      weight,
+      value;
 
-  return value;
+  while (capacity != 0 && densities.size() != 0)
+  {
+    //nlogn, gets max and then entry removed from map
+    index = getGreatestDensity(densities);
+
+    weight = weights[index];
+    value = values[index];
+
+    if (weight <= capacity)
+    {
+      capacity -= weight;
+      totalValue += value;
+    }
+    else
+    {
+      totalValue += capacity * densities[index];
+      capacity = 0;
+    }
+
+    densities.erase(index);
+  }
+
+  return totalValue;
 }
 
 int main()
 {
   int n;
-  int capacity;
+  long capacity;
   cin >> n >> capacity;
-  vector<int> values(n);
-  vector<int> weights(n);
+  vector<long> values(n);
+  vector<long> weights(n);
   for (int i = 0; i < n; i++)
   {
     cin >> values[i] >> weights[i];
   }
-
-  double optimal_value = get_optimal_value(capacity, weights, values);
-
-  cout.precision(10);
-  cout << optimal_value << endl;
+  cout.precision(8);
+  cout << round(get_optimal_value(capacity, weights, values) * 10000.0) / 10000.0 << endl;
   return 0;
 }
