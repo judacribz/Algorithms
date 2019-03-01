20492d091e5b2d8a8b14f8ac2d549f927ca98a51
judacribz <sheron.balas@gmail.com>
2019-03-01 14:29:38 -0500

    finished 1 - money change prob

--- week3/change.cpp
+++ week3/change.cpp
@@ -1,12 +1,35 @@
 #include <iostream>
 
-int get_change(int m) {
-  //write your code here
-  return n;
+using namespace std;
+
+const int COINS[3] = {10, 5, 1};
+
+int get_change(int m, int coinInd)
+{
+  if (m == 0)
+  {
+    return 0;
+  }
+
+  int coin = COINS[coinInd];
+
+  // in case COINS array does not have 1s
+  if (coinInd == sizeof(COINS) / sizeof(coin) - 1)
+  {
+    return m / coin;
+  }
+
+  return m / coin + get_change(m % coin, coinInd + 1);
 }
 
-int main() {
+int get_change(int m)
+{
+  return get_change(m, 0);
+}
+
+int main()
+{
   int m;
-  std::cin >> m;
-  std::cout << get_change(m) << '\n';
+  cin >> m;
+  cout << get_change(m) << '\n';
 }
