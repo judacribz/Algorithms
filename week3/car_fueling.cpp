0d894c2bdc5134a29d798c6398629b05578097c1
judacribz <sheron.balas@gmail.com>
2019-03-01 16:05:20 -0500

    finished 3 - car fueling problem

--- week3/car_fueling.cpp
+++ week3/car_fueling.cpp
@@ -1,28 +1,59 @@
 #include <iostream>
 #include <vector>
 
-using std::cin;
-using std::cout;
-using std::vector;
-using std::max;
+using namespace std;
 
-int compute_min_refills(int dist, int tank, vector<int> & stops) {
-    // write your code here
-    return -1;
+int compute_min_refills(int dist, int tank, vector<long> &stops)
+{
+    long
+        totalFills = 0,
+        lastStation = 0,
+        lastStopped = 0;
+
+    for (vector<long>::iterator it = stops.begin(); it != stops.end(); it++)
+    {
+        if (tank > *it - lastStopped)
+        {
+            lastStation = *it;
+            continue;
+        }
+        else
+        {
+            if (lastStation == lastStopped)
+            {
+                return -1;
+            }
+            else
+            {
+                totalFills++;
+                lastStopped = lastStation;
+            }
+        }
+    }
+    return totalFills;
 }
 
-
-int main() {
-    int d = 0;
+int main()
+{
+    // distance to city
+    long d = 0;
     cin >> d;
+
+    // full tank distance
     int m = 0;
     cin >> m;
+
+    // num gas stations on the way
     int n = 0;
     cin >> n;
 
-    vector<int> stops(n);
+    vector<long> stops(n);
     for (size_t i = 0; i < n; ++i)
+    {
         cin >> stops.at(i);
+    }
+
+    stops.push_back(d);
 
     cout << compute_min_refills(d, m, stops) << "\n";
 
