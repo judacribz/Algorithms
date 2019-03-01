e35bdbdc49853afc6733e4c238e9f61aa93bcdcb
judacribz <sheron.balas@gmail.com>
2019-03-01 14:47:17 -0500

    finished 1 - money change prob

--- week3/fractional_knapsack.cpp
+++ week3/fractional_knapsack.cpp
@@ -1,29 +1,39 @@
 #include <iostream>
 #include <vector>
+#include <map>
 
-using std::vector;
+using namespace std;
 
-double get_optimal_value(int capacity, vector<int> weights, vector<int> values) {
+double get_optimal_value(int capacity, vector<int> weights, vector<int> values)
+{
   double value = 0.0;
+  vector<float> densities;
 
-  // write your code here
+  for (int i = 0; i < weights.size(); i++)
+  {
+    densities[i] = (float)values[i] / weights[i];
+  }
+
+  cout << densities << endl;
 
   return value;
 }
 
-int main() {
+int main()
+{
   int n;
   int capacity;
-  std::cin >> n >> capacity;
+  cin >> n >> capacity;
   vector<int> values(n);
   vector<int> weights(n);
-  for (int i = 0; i < n; i++) {
-    std::cin >> values[i] >> weights[i];
+  for (int i = 0; i < n; i++)
+  {
+    cin >> values[i] >> weights[i];
   }
 
   double optimal_value = get_optimal_value(capacity, weights, values);
 
-  std::cout.precision(10);
-  std::cout << optimal_value << std::endl;
+  cout.precision(10);
+  cout << optimal_value << endl;
   return 0;
 }
