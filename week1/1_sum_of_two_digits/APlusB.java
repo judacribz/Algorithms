95851a13ae0ab392a07895555d60ade5e9b3cc8e
judacribz <sheron.balas@gmail.com>
2019-04-12 11:51:44 -0400

    added shell script 'run' to compile and run both java and cpp files

--- week1/1_sum_of_two_digits/APlusB.java
+++ week1/1_sum_of_two_digits/APlusB.java
@@ -9,6 +9,7 @@ class APlusB {
         Scanner s = new Scanner(System.in);
         int a = s.nextInt();
         int b = s.nextInt();
+        s.close();
         System.out.println(sumOfTwoDigits(a, b));
     }
 }
